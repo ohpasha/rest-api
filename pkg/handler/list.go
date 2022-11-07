87ac612bf98506ac051e0c45f6c017c22733d871
shcherbakov <shcherbakov@banki.ru>
2022-11-08 01:29:13 +0400

    add gettting list by id

--- pkg/handler/list.go
+++ pkg/handler/list.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	todo "github.com/ohpasha/rest-api"
@@ -61,7 +62,31 @@ func (h *Handler) getAllLists(c *gin.Context) {
 }
 
 func (h *Handler) getListById(c *gin.Context) {
+	userId, ok := getUserId(c)
 
+	if ok != nil {
+		NewErrorResponse(c, http.StatusInternalServerError, "no userId in context")
+
+		return
+	}
+
+	id, err := strconv.Atoi(c.Param("id"))
+
+	if err != nil {
+		NewErrorResponse(c, http.StatusBadRequest, "id not integer")
+
+		return
+	}
+
+	list, err := h.services.TodoList.GetById(userId, id)
+
+	if err != nil {
+		NewErrorResponse(c, http.StatusInternalServerError, err.Error())
+
+		return
+	}
+
+	c.JSON(http.StatusOK, list)
 }
 
 func (h *Handler) updateList(c *gin.Context) {
